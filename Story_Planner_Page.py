32c7429657d5e1f22ed4796d39fde444f2181b97
Tilda <tilda_gefion@icloud.com>
2025-07-17 09:32:57 +0200

    Edited Story Planner Page

--- Story_Planner_Page.py
+++ Story_Planner_Page.py
@@ -29,21 +29,60 @@ with col2:
 st.caption(f"📁 Current working directory: `{os.getcwd()}`")
 
 if st.session_state.mode == "edit":
+    number = st.selectbox("Input here:", (1, 2, 3, 4, 5))
     with st.form("Beginning of the Story", clear_on_submit=True):
         text = st.text_input("Input the first sentence of the game", value="You find yourself on a lively market")
-        choice1 = st.text_input("Choice 1", value="Check out the vendors at the market")
-        consq1 = st.text_input("Consequence 1", value="One vendor makes buy smth (-50g, +'rusty sword')")
-        choice2 = st.text_input("Choice 2", value="Plan the adventure in the Tavern at the corner")
-        consq2 = st.text_input("Consequence 2", value="*Option buttons: location 1, location2, ...*")
+        if number == 1:
+            choice = st.text_input("Choice 1")
+            consq = st.text_input("Consequence 1")
+        elif number == 2:
+            choice = st.text_input("Choice 1")
+            consq = st.text_input("Consequence 1")
+            choice2 = st.text_input("Choice 2")
+            consq2 = st.text_input("Consequence 2")
+        elif number == 3:
+            choice = st.text_input("Choice 1")
+            consq = st.text_input("Consequence 1")
+            choice2 = st.text_input("Choice 2")
+            consq2 = st.text_input("Consequence 2")
+            choice3 = st.text_input("Choice 3")
+            consq3 = st.text_input("Consequence 3")
+        elif number == 4:
+            choice = st.text_input("Choice 1")
+            consq = st.text_input("Consequence 1")
+            choice2 = st.text_input("Choice 2")
+            consq2 = st.text_input("Consequence 2")
+            choice3 = st.text_input("Choice 3")
+            consq3 = st.text_input("Consequence 3")
+            choice4 = st.text_input("Choice 4")
+            consq4 = st.text_input("Consequence 4")
+        elif number == 5:
+            choice = st.text_input("Choice 1")
+            consq = st.text_input("Consequence 1")
+            choice2 = st.text_input("Choice 2")
+            consq2 = st.text_input("Consequence 2")
+            choice3 = st.text_input("Choice 3")
+            consq3 = st.text_input("Consequence 3")
+            choice4 = st.text_input("Choice 4")
+            consq4 = st.text_input("Consequence 4")
+            choice5 = st.text_input("Choice 5")
+            consq5 = st.text_input("Consequence 5")
         save = st.form_submit_button("Save 'Beginning of the Story'")
 
         if save:
             datas1 = {
                 "Sentence": text,
-                "Choice1": choice1,
-                "Consequence1": consq1,
+                "Choice1": choice,
+                "Consequence1": consq,
                 "Choice2": choice2,
-                "Consequence2": consq2
+                "Consequence2": consq2,
+                "Choice3": choice3,
+                "Consequence3": consq3,
+                "Choice4": choice4,
+                "Consequence4": consq4,
+                "Choice5": choice5,
+                "Consequence5": consq5,
+
             }
 
             save_element(datas1, file_beg)
